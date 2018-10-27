28c5c3917fff98282448b5299b0255bdac0cbd60
Wyatt Joyner <wcj3te@virginia.edu>
2018-10-27 00:15:39 -0400

    Sloths of stuff added

---
+++ Slost/Assets/LimbLine.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimbLine : MonoBehaviour {
+
+    private LineRenderer limbLine;
+    public Transform socket;
+    private Rigidbody2D body;
+
+    private Vector3 newLocation;
+    private Vector3 circleCenter;
+    private float circleRadius;
+
+    public int lastIndex;
+
+	// Use this for initialization
+	void Start () {
+        //socket = transform.parent;
+        limbLine = GetComponent<LineRenderer>();
+        circleCenter = transform.parent.position;
+        circleRadius = 2f;
+        body = GetComponent<Rigidbody2D>();
+        //transform.position = circleCenter;
+        newLocation = transform.position;
+        //GetComponent<Rigidbody2D>().AddForce(new Vector2(500, 0));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        limbLine.SetPosition(lastIndex, transform.position);
+        limbLine.SetPosition(0, socket.position);
+    }
+}
---
+++ Slost/Assets/LimbSegment.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimbSegment : MonoBehaviour {
+
+    public LineRenderer limbLine;
+    public int index;
+	
+	// Update is called once per frame
+	void Update () {
+        limbLine.SetPosition(index, transform.position);
+	}
+}
